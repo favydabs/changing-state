d4435fe5e39f370fa0afbe9f955f954d5dc1be20
favydabs <favourdabit@gmail.com>
2022-06-25 15:51:48 +0100

    third stage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,45 @@
 import React, { useState } from "react";
 
 function App() {
-  const [fname, setFname] = useState("");
-  const [lname, setLname] = useState("");
+  const [fullname, setFullname] = useState({
+    fName: "",
+    lName: ""
+  });
 
-  function updateFname(event) {
-    setFname(event.target.value);
+  function handleChange(event) {
+    const newValue = event.target.value;
+    const inputName = event.target.name;
+
+    setFullname(prevValue => {
+      if (inputName === 'fName') {
+        return {
+          fName: newValue,
+          lName: prevValue.lName
+        }
+      } else if (inputName === 'lName') {
+        return {
+          fName: prevValue.fName,
+          lName: newValue
+        }
+      }
+    })
   }
-  function updateLname(event) {
-    setLname(event.target.value);
-  }
+  
   return (
     <div className="container">
-      <h1>Hello {fname} {lname}</h1>
+      <h1>Hello {fullname.fName} {fullname.lName}</h1>
       <form>
         <input
-        onChange={updateFname}
-         name="fName"
+          onChange={handleChange}
+          name="fName"
           placeholder="First Name"
-           value={fname}
-            />
+          value={fullname.fName}
+        />
         <input
-        onChange={updateLname} 
-        name="lName" 
-        placeholder="Last Name" 
-        value={lname} 
+          onChange={handleChange}
+          name="lName"
+          placeholder="Last Name"
+          value={fullname.lName}
 
         />
         <button>Submit</button>
