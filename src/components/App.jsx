57006af33f62025623b6b71b51d6a421f414026c
favydabs <favourdabit@gmail.com>
2022-06-25 15:05:06 +0100

    second stage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
+
+  function updateFname(event) {
+    setFname(event.target.value);
+  }
+  function updateLname(event) {
+    setLname(event.target.value);
+  }
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>Hello {fname} {lname}</h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input
+        onChange={updateFname}
+         name="fName"
+          placeholder="First Name"
+           value={fname}
+            />
+        <input
+        onChange={updateLname} 
+        name="lName" 
+        placeholder="Last Name" 
+        value={lname} 
+
+        />
         <button>Submit</button>
       </form>
     </div>
